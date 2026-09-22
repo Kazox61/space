d8728c8aab81a3bd1e1f0a554f772f4153b2f5de
Kazox61 <pawelka00florian@gmail.com>
2026-09-22 16:46:30 +0200

    Fix one-shot particle restart

--- Client/assets/player/vfx/OneShotParticles.cs
+++ Client/assets/player/vfx/OneShotParticles.cs
@@ -4,8 +4,9 @@ namespace Space.Client;
 
 public partial class OneShotParticles : GpuParticles3D {
 	public override void _Ready() {
+		Emitting = false;
 		OneShot = true;
 		Finished += QueueFree;
-		Emitting = true;
+		Restart();
 	}
 }
